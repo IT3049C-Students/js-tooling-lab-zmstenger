3774a410a66262b3b9a4be6fdbf8c98bddc84ee6
Zach <stengezm@mail.uc.edu>
2022-07-14 17:10:55 -0400

    fix

--- cypress/e2e/1-getting-started/new-test.cy.js
+++ cypress/e2e/1-getting-started/new-test.cy.js
@@ -7,28 +7,16 @@ describe('Rock Paper Scissors', ()=>{
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
     cy.visit(`https://it3049c-students.github.io/js-tooling-lab-zmstenger/`)
-    cy.get('.action-email')
-      .type('fake@email.com').should('have.value', 'fake@email.com')
+    // Type Name
+    cy.get('.form-control')
+      .type('Zachary Stenger')
+    // Select Choice
+    cy.get('.custom-select')
+      .select('rock')
+    // Press Go
+    cy.get('.btn-success').click()
+    //cy.get()
 
-      // .type() with special character sequences
-      .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
-      .type('{del}{selectall}{backspace}')
-
-      // .type() with key modifiers
-      .type('{alt}{option}') //these are equivalent
-      .type('{ctrl}{control}') //these are equivalent
-      .type('{meta}{command}{cmd}') //these are equivalent
-      .type('{shift}')
-
-      // Delay each keypress by 0.1 sec
-      .type('slow.typing@email.com', { delay: 100 })
-      .should('have.value', 'slow.typing@email.com')
-
-    cy.get('.action-disabled')
-      // Ignore error checking prior to type
-      // like whether the input is visible or disabled
-      .type('disabled error checking', { force: true })
-      .should('have.value', 'disabled error checking')
+    })
+    
   })
-
-})
--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -13,58 +13,69 @@ export class RockPaperScissors {
    * using Math.random() method, you should be able to get one of the following values
    */
   generateCPUResponse(){
-    let acceptedValues = [ `rock`, `paper`, `scissors` ];
-    const randomIndexNumber = Math.floor(Math.random() * 3); 
-
-    return acceptedValues[randomIndexNumber];
+    const acceptedValues = [ `rock`, `paper`, `scissors` ];
+    return acceptedValues[Math.floor(Math.random() * 3)];
   }
   /**
-   * returns one of the following values: `win`, `lose`, `tie`
-   * tie:
-   *     the user selection the same as the CPU
-   * win: 
-   *    (user is `rock` and cpu is `scissors
-   *     OR
-   *    (user is `paper` and cpu is `rock`) 
-   *     OR 
-   *    (user is `scissors` and cpu is `paper`)
-   * `lose`:
-   *    the opposite case :)
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    * @param {string} cpuSelection computer selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   determineWinner(userSelection, cpuSelection){
-    if (userSelection ===cpuSelection) { 
-      return `tie`; 
+    if (userSelection === cpuSelection) {
+      return "tie";
     }
-    else if(
-      (userSelection ===`paper` && cpuSelection ===`rock`) || 
-      (userSelection ===`scissors` && cpuSelection ===`paper`) || 
-      (userSelection ===`rock` && cpuSelection ===`scissors`)
-    ) { 
-      return `win`; 
-    }
-    else { 
-      return `lose`; 
-    }
-  }
 
+    if (userSelection === "rock") {
+      if (cpuSelection === "scissors") {
+        return "win"
+      } else {
+        return "lose"
+      }
+    }
+    //Check for Paper
+    else if (userSelection === "paper") {
+      if (cpuSelection === "rock") {
+       return "win"
+      } else {
+        return "lose"
+      }
+    }
+    //Check for Scissors
+    else if(userSelection == "scissors"){
+      if (cpuSelection === "paper") {
+        return "win"
+      } else {
+        return "lose"
+      }
+    }
+
+  else{
+    return "lose"
+  }
+} 
   /**
-   * 
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   play(userSelection){
-    const cpuSelection = this.generateCPUResponse(); 
-    const results = this.determineWinner(userSelection, cpuSelection); 
-    
-    if(results ===`win`) { 
-      this.score.user++; 
-    }
-    if(results ===`lose`) { 
-      this.score.cpu++; 
-    }
+    const cpuResponse = this.generateCPUResponse()
+    if(this.determineWinner(userSelection, cpuResponse) == "win")
+    {
+      this.score.user++
+      this.gameHistoryLog.push(`${this.username} selected ${userSelection}, CPU selected ${cpuResponse}: ${this.username} wins`.toString());
 
-    this.gameHistoryLog.push(` ${this.username} selected ${userSelection}. CPU selected ${cpuSelection}: ${this.username} ${results}s`);
+    }
+    
+    else if(this.determineWinner(userSelection, cpuResponse)=="lose")
+    {
+      this.score.cpu++
+      this.gameHistoryLog.push(`${this.username} selected ${userSelection}, CPU selected ${cpuResponse}: CPU wins`.toString());
+
+    }
+    else if(this.determineWinner(userSelection, cpuResponse)=="tie")
+    {
+      this.gameHistoryLog.push(`${this.username} selected ${userSelection}, CPU selected ${cpuResponse}: ${this.username} ties with CPU`.toString());
+    }
+    //alert(this.determineWinner(userSelection, cpuResponse))
   }
 
 }
